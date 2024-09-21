c562264fb5b2deb383c3796988ef421fc10cf590
Mate Tohai <admin@Tmate6.com>
2024-09-21 16:57:55 +0300

    Initial Commit

---
+++ NewFolderFile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewFolderFile
+//
+//  Created by Mate Tohai on 21/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewFolderFile/NewFolderFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewFolderFileApp.swift
+//  NewFolderFile
+//
+//  Created by Mate Tohai on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewFolderFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
